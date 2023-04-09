0018026e64ccd4ffc7f02d2ee518e79674d8b890
Wesley Hovick <wesley.hovick@gmail.com>
2023-04-09 01:16:30 -0700

    attempt fix cache issue

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     path: "/public/"
-  }
+  },
+  generateEtags: false
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,15 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Rocket League Training Calendar</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
