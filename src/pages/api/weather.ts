026577b39e6a792f3907e135554a045a290e16dc
Wesley Hovick <wesley.hovick@gmail.com>
2023-04-16 15:11:05 -0700

    update deploy cfg

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,13 +1,20 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../../lib/mongodb";
+import { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from '../../../lib/mongodb'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     const client = await clientPromise
-    const db = client.db("sample_weatherdata")
+    const db = client.db('sample_weatherdata')
     switch (req.method) {
-        case "GET":
-            const allWeatherData = await db.collection("data").find({}).limit(5).toArray();
+        case 'GET':
+            const allWeatherData = await db
+                .collection('data')
+                .find({})
+                .limit(5)
+                .toArray()
             res.json({ status: 200, data: allWeatherData })
-            break;
+            break
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,26 @@ import { BaseLayout } from '@/components/BaseLayout'
 import { InferGetServerSidePropsType } from 'next'
 
 export async function getServerSideProps() {
-    let res = await fetch("http://localhost:3000/api/weather", {
-        method: "GET",
+    let res = await fetch('http://localhost:3000/api/weather', {
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
         },
     })
 
-    const allWeather = await res.json();
+    const allWeather = await res.json()
 
     return {
         props: {
-            allWeather
+            allWeather,
         },
     }
 }
 
 export default function Home({
-    allWeather
+    allWeather,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    console.log({allWeather})
+    console.log({ allWeather })
 
     return <BaseLayout />
 }
