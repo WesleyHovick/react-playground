1c03a411ada3dec2e283858f6f60e981e5ca66f5
Wesley Hovick <wesley.hovick@gmail.com>
2023-04-16 11:21:15 -0700

    cleanup main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,19 +1,6 @@
 import styled from '@emotion/styled'
 import { Container, Paper, Typography } from '@mui/material'
 
-const CustomPaper = styled(Paper)`
-    height: 40px;
-    //   background-color: white;
-`
-
-const MainContainer = styled.div`
-    margin-left: 240px;
-    border: 1px solid white;
-    margin-top: 128px;
-`
-
-const Header = styled.header``
-
 interface IMain {
     children?: React.ReactNode
 }
